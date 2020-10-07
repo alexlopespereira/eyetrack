0253f1c9076494bef709354d61a3d4bd85b6edcc
alex <alex.pereira@planejamento.gov.br>
2020-10-06 23:57:39 -0300

    debugando com imagens

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ import numpy as np
 from screeninfo import get_monitors
 from util import RingBuffer
 import pyautogui
+pyautogui.FAILSAFE = False
 
 s0x, s0y = pyautogui.position()
 
-def exp_function(x):
-    alpha = 0.005
-    beta = 0.4
-    ret = beta * math.exp(alpha * x - 1)
+def exp_function(x, axis=0):
+    if axis == 0:
+        alpha = 0.056137917
+        beta = 2.501428786
+    else:
+        alpha = 0.0091
+        beta = 2.488
+    # ret = beta * np.exp(alpha * x - 1)
+    ret = x #math.pow(x, 1.7)/50
     return ret
 
 
-def calc_displacements(s0, s1, inc):
-    if s0 == s1:
-        return 0
-    d = exp_function(abs(s0 - s1) * inc)
-    if s1 < s0:
-        d = -d
+def calc_displacements(s, inc, axis=0):
+    d = exp_function(s, axis) * inc
+    # if s1 > s0:
+    #     d = -d
     return d
 
 
@@ -34,7 +38,7 @@ for m in get_monitors():
     total_W += m.width
 print(total_H)
 print(total_W)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 _, frame = cap.read()
 h = frame.shape[0]
 w = frame.shape[1]
@@ -47,13 +51,13 @@ yhistory = RingBuffer(ringlen)
 last_dist_x = last_dist_y = 0
 firstx = True
 firsty = True
+count = 0
 
-
-# while True:
-#     _, frame = cap.read()
-images = glob.glob('data/*.png')
-for fname in images:
-    frame = cv2.imread(fname)
+while True:
+    _, frame = cap.read()
+# images = sorted(glob.glob('data/*.png'))
+# for fname in images:
+#     frame = cv2.imread(fname)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, corners = cv2.findChessboardCorners(gray, (3, 3), None, flags=cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE
@@ -62,21 +66,21 @@ for fname in images:
     desty = s0y
     if ret == True:
         m = cv2.mean(corners)
-        xhistory.extend(np.array([m[1]]))
-        yhistory.extend(np.array([m[0]]))
+        xhistory.extend(np.array([m[0]]))
+        yhistory.extend(np.array([m[1]]))
         if not firstx:
             dx = xhistory.get()
             first_pointx = np.nonzero(dx)[0][0]
             dispx = dx[-1] - dx[first_pointx]
             curr_dist_x = abs(dispx)
-            if curr_dist_x + turning_th < last_dist_x:
+            if len(np.nonzero(dx)[0]) > 1 and (curr_dist_x + turning_th < abs(dx[-2] - dx[first_pointx])):
                 xhistory = RingBuffer(ringlen)
                 firstx = True
                 s0x = (s0x - curr_dist_x) if dispx > 0 else (s0x + curr_dist_x)
                 last_dist_x = 0
             else:
                 last_dist_x = curr_dist_x
-            incdx = calc_displacements(s0x, curr_dist_x, incx)
+            incdx = calc_displacements(curr_dist_x, incx, axis=0)
             destx = s0x + incdx
 
         if not firsty:
@@ -84,30 +88,30 @@ for fname in images:
             first_pointy = np.nonzero(dy)[0][0]
             dispy = dy[-1] - dy[first_pointy]
             curr_dist_y = abs(dispy)
-            if curr_dist_y + turning_th < last_dist_y:
+            if len(np.nonzero(dx)[0]) > 1 and (curr_dist_y < abs(dy[-2] - dy[first_pointy])):
                 yhistory = RingBuffer(ringlen)
                 firsty = True
                 s0y = (s0y - curr_dist_y) if dispy > 0 else (s0y + curr_dist_y)
                 last_dist_y = 0
             else:
                 last_dist_y = curr_dist_y
-            incdy = calc_displacements(s0y, curr_dist_y, incy)
+            incdy = calc_displacements(curr_dist_y, incy, axis=1)
             desty = s0y + incdy
 
 
-        adjx = max(0, min(destx, total_W-1))
-        adjy = max(0, min(desty, total_H-1))
+        adjx = int(max(1, min(destx, total_W-1)))
+        adjy = int(max(1, min(desty, total_H-1)))
         print(destx, desty, adjx, adjy)
-        # pyautogui.moveTo(adjx, adjy)
+        pyautogui.moveTo(adjx, adjy)
         # Draw and display the corners
         img = cv2.drawChessboardCorners(frame, (3, 3), corners, ret)
-        cv2.imshow("corners", img)
+        # cv2.imshow("corners", img)
 
     cv2.imshow("Frame", frame)
-    cv2.imshow("gray", gray)
+    # cv2.imshow("gray", gray)
     firstx = False
     firsty = False
-
+    count += 1
     key = cv2.waitKey(1)
     if key == 27:
         break
